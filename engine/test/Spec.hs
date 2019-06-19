146dee553961d01b9107c6890a872704fbb7b518
Michael Gilliland <mjg.py3@gmail.com>
2019-06-18 22:24:50 -0400

    Add HSpec framework for specs.

--- engine/test/Spec.hs
+++ engine/test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "todo engine" $
+    it "doesn't have much yet" $ do
+      pending
