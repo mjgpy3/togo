ed1d3e53425fe45105480d409105de4f0acb37a0
Michael Gilliland <mjg.py3@gmail.com>
2019-06-19 20:23:59 -0400

    Add basic text rendering.

--- engine/src/Core.hs
+++ engine/src/Core.hs
@@ -4,22 +4,35 @@ module Core
   , gameOf
   , Stone(..)
   , Event(..)
+  , GameSize(..)
+  , State
+  , widthAndHeight
+  , pieceAt
   ) where
 
 import qualified Data.Map.Strict as M
 
 data Stone = Black | White deriving (Eq, Show)
 
+data GameSize = Standard deriving (Eq, Show)
+
 data Event = StonePlaced Stone (Int, Int)
-type State = M.Map (Int, Int) Stone
+type State = (M.Map (Int, Int) Stone, GameSize)
+
+widthAndHeight :: State -> (Int, Int)
+widthAndHeight (_, Standard) = (13, 13)
+
+pieceAt :: (Int, Int) -> State -> Maybe Stone
+pieceAt point (game, _) = M.lookup point game
 
 emptyGame :: State
-emptyGame = M.empty
+emptyGame = (M.empty, Standard)
 
 gameOf :: [((Int, Int), Stone)] -> State
-gameOf = M.fromList
+gameOf vs = (M.fromList vs, Standard)
 
 summarize :: [Event] -> State
-summarize [] = emptyGame
-summarize (StonePlaced color pos:rest) =
-  M.insert pos color $ summarize rest
+summarize = standardSized . foldr track M.empty
+  where
+    track (StonePlaced color pos) board = M.insert pos color board
+    standardSized board = (board, Standard)
---
+++ engine/src/Render.hs
@@ -0,0 +1,24 @@
+module Render
+  ( render
+  , blackPiece
+  , whitePiece
+  ) where
+
+import Core (widthAndHeight, State, Stone(..), pieceAt)
+import Data.List (intersperse)
+
+render :: State -> String
+render state = do
+  y <- [0..snd (widthAndHeight state)-1]
+  (intersperse '-' $ genRow y) ++ "\n"
+
+  where
+    genRow y = do
+      x <- [0..fst (widthAndHeight state)-1]
+      case pieceAt (x, y) state of
+         Just White -> [whitePiece]
+         Just Black -> [blackPiece]
+         _ -> "+"
+
+blackPiece = '○'
+whitePiece = '⏺'
