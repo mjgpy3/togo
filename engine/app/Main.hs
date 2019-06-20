e730ee4d7d2430df5947007f1137e7e046b8e554
Michael Gilliland <mjg.py3@gmail.com>
2019-06-19 21:57:38 -0400

    Worlds most basic engine.

--- engine/app/Main.hs
+++ engine/app/Main.hs
@@ -1,6 +1,44 @@
 module Main where
 
-import Lib
+import Commands (execute, Command(..))
+import Core (emptyGame, turn, State, widthAndHeight, Event)
+import Render (render)
+import System.Console.ANSI (clearScreen)
+import Text.Read (readMaybe)
+
+inBoardBoundaries :: (Int, Int) -> State -> Bool
+inBoardBoundaries (x, y) state =
+  let (width, height) = widthAndHeight state
+  in 1 <= x && x <= width && 1 <= y && y <= height
+
+parseCommand :: State -> IO Command
+parseCommand state = do
+  line <- getLine
+  let xText = takeWhile (/= ' ') line
+  let yText = dropWhile (/= ' ') line
+  case (readMaybe xText, readMaybe yText) of
+    (Just x, Just y) ->
+      if inBoardBoundaries (x, y) state
+      then pure (Place (turn state) (x-1, y-1))
+      else do
+        putStrLn (line ++ " are not within the expected boundaries " ++ show (widthAndHeight state))
+        parseCommand state
+    _ -> do
+      putStrLn $ "Expected two numbers but got " ++ line
+      parseCommand state
+
+game :: State -> [Event] -> IO ()
+game state events = do
+  clearScreen
+  putStrLn (show (turn state) ++ "'s turn")
+  putStrLn $ render state
+  command <- parseCommand state
+  case execute command state of
+    Left _ -> do
+      putStrLn "An unexpected error occured while running command"
+      game state events
+    Right (newState, newEvents) ->
+      game newState (newEvents ++ events)
 
 main :: IO ()
-main = someFunc
+main = game emptyGame []
--- engine/src/Commands.hs
+++ engine/src/Commands.hs
@@ -1,16 +1,16 @@
 module Commands
   ( execute
-  , Commmand(..)
+  , Command(..)
   ) where
 
 import Core (State, Event(..), Stone, Position, track)
 
-data Commmand = Place Stone Position
+data Command = Place Stone Position
 
-execute :: Commmand -> State -> Either () (State, [Event])
+execute :: Command -> State -> Either () (State, [Event])
 execute command state = do
   events <- executeEvents command state
   pure (foldr track state events, events)
 
-executeEvents :: Commmand -> State -> Either () [Event]
+executeEvents :: Command -> State -> Either () [Event]
 executeEvents (Place s p) _ = Right [StonePlaced s p]
