dbd45b8e42b4b20ed4f81aa3df0b23af3d771b8b
Michael Gilliland <mjg.py3@gmail.com>
2019-07-04 00:42:57 -0400

    Super trivial highlighting (it's not too great...)

--- ui/src/Game.purs
+++ ui/src/Game.purs
@@ -14,6 +14,7 @@ import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Web.Event.Event (Event)
 import Web.Event.Event as Event
+import Web.UIEvent.MouseEvent as ME
 import Data.Argonaut.Core as A
 import Data.Argonaut.Decode (class DecodeJson, decodeJson, (.:))
 import Halogen.HTML.CSS (style)
@@ -24,6 +25,9 @@ newtype Position
   = Pos { x :: Int
         , y :: Int
         }
+
+instance eqPosition :: Eq Position where
+  eq (Pos a) (Pos b) = a.x == b.x && a.y == b.y
 
 data Stone
   = Black
@@ -40,13 +44,24 @@ newtype Game
          , over :: Boolean
          }
 
-data State
+type Match = { matchId :: MatchIdentifier
+             , game :: Game
+             , currentHighlight :: Maybe Position
+             }
+
+data State a
   = NoneYet
   | Loading
   | UnexpectedError
-  | LocalMatch MatchIdentifier Game
+  | LocalMatch a
 
 data MatchIdentifier = MatchIdentifier String
+
+instance functorGameState :: Functor State where
+  map _ NoneYet = NoneYet
+  map _ Loading = Loading
+  map _ UnexpectedError = UnexpectedError
+  map f (LocalMatch v) = LocalMatch $ f v
 
 instance decodeJsonPosition :: DecodeJson Position where
   decodeJson json = do
@@ -90,6 +105,8 @@ instance decodeJsonMatchIdentifier :: DecodeJson MatchIdentifier where
 
 data Action
   = StartNewLocalMatch Event
+  | HighlightStone Position
+  | ClearHighlightedStone
 
 component :: forall f i o m. MonadAff m => H.Component HH.HTML f i o m
 component =
@@ -99,10 +116,10 @@ component =
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
 
-initialState :: forall i. i -> State
+initialState :: forall i. i -> State Match
 initialState _ = NoneYet
 
-render :: forall m. State -> H.ComponentHTML Action () m
+render :: forall m. State Match -> H.ComponentHTML Action () m
 render NoneYet =
   HH.form
     [ HE.onSubmit (Just <<< StartNewLocalMatch) ]
@@ -115,10 +132,10 @@ render NoneYet =
     ]
 render Loading = HH.p_ [ HH.text "Loading..." ]
 render UnexpectedError = HH.p_ [ HH.text "An unexpected error has occured, please try again later..." ]
-render (LocalMatch _ game) = renderGame game
+render (LocalMatch m) = renderGame m
 
-renderGame :: forall m. Game -> H.ComponentHTML Action () m
-renderGame (Game g) =
+renderGame :: forall m. Match -> H.ComponentHTML Action () m
+renderGame { game: Game g, currentHighlight } =
   HH.table [ style do CSS.key (CSS.fromString "border-spacing") (CSS.px 0.0)
                       CSS.border CSS.solid (CSS.px 2.0) (CSS.graytone 0.2) 
                       CSS.backgroundColor (fromMaybe (CSS.graytone 0.5) $ CSS.fromHexString "#966F33") ] $ do
@@ -127,16 +144,48 @@ renderGame (Game g) =
       x <- Array.range 0 g.width
       pure $ HH.td [ style do CSS.border CSS.solid (CSS.px 2.0) CSS.black
                               CSS.height (CSS.rem 2.5)
-                              CSS.width (CSS.rem 2.5)
-                               ]
-        []
+                              CSS.width (CSS.rem 2.5) ]
+        [ HH.table [ style do CSS.key (CSS.fromString "border-spacing") (CSS.px 0.0)
+                              CSS.height (CSS.pct 100.0)
+                              CSS.width (CSS.pct 100.0)
+                   ]
+            (
+              let
+                highlightable x' y' = [ HE.onMouseOver (Just <<< const (HighlightStone $ Pos { x: x', y: y' }))
+                                      , HE.onMouseOut (Just <<< const ClearHighlightedStone)
+                                      ]
+
+                withinBounds x' y' =
+                  0 <= x' && x' < g.width && 0 <= y' && y' < g.height
+
+                highlighted x' y' =
+                  Just (Pos { x: x', y: y' }) == currentHighlight
+
+                highlight = [ style do CSS.backgroundColor (fromMaybe (CSS.graytone 0.5) $ CSS.fromHexString "#FF0") ]
+
+                chunkAt x' y' =
+                  HH.td ((if withinBounds x' y' then highlightable x' y' else []) <> (if highlighted x' y' then highlight else [])) []
+              in
+                [ HH.tr_ [ chunkAt (x - 1) (y - 1)
+                         , chunkAt x (y - 1)
+                         ]
+                , HH.tr_ [ chunkAt (x - 1) y
+                         , chunkAt x y
+                         ]
+                ]
+            )
+        ]
 
 
 noContent :: AXRB.RequestBody
 noContent = AXRB.string ""
 
-handleAction :: forall o m. MonadAff m => Action -> H.HalogenM State Action () o m Unit
+handleAction :: forall o m. MonadAff m => Action -> H.HalogenM (State Match) Action () o m Unit
 handleAction = case _ of
+  ClearHighlightedStone -> do
+    H.modify_ $ map (\m -> m { currentHighlight = Nothing })
+  HighlightStone pos -> do
+    H.modify_ $ map (\m -> m { currentHighlight = Just pos })
   StartNewLocalMatch event -> do
     H.liftEffect $ Event.preventDefault event
     H.put Loading
@@ -148,7 +197,7 @@ handleAction = case _ of
         case justDecode gameResponse.body of
           Nothing -> H.put UnexpectedError
           Just game ->
-            H.put $ LocalMatch (MatchIdentifier matchId) game
+            H.put $ LocalMatch { matchId: MatchIdentifier matchId, currentHighlight: Nothing, game }
 
   where
     justDecode :: forall v e. DecodeJson v => Either e A.Json -> Maybe v
