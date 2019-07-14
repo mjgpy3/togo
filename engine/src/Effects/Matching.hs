f440af51f44b0f08ee5984be9a8f4620dbd63395
Michael Gilliland <mjg.py3@gmail.com>
2019-07-13 23:49:41 -0400

    Related to #37: try to make DB directory configurable.

--- engine/src/Effects/Matching.hs
+++ engine/src/Effects/Matching.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Effects.Matching
   ( Matching(..)
@@ -20,8 +21,11 @@ import Data.Aeson
 import System.Directory
 import Data.List (sortOn)
 import qualified Data.Ord as Ord
-import Data.Maybe (mapMaybe)
+import Data.Maybe (mapMaybe, fromMaybe)
 import Text.Read (readMaybe)
+import Shelly (mkdir_p, shelly, fromText)
+import System.Environment (lookupEnv)
+import Data.Text (pack)
 
 data Matching m a where
   CreateMatch :: Matching m M.Match
@@ -45,34 +49,44 @@ runMatchWithFileSystemStore = interpret $ \case
   GetMatch matchId -> sendM $ toMatchIfExists matchId
 
   where
-    matchDir :: M.Match -> String
-    matchDir match = "/tmp/togo/" ++ M.identifier match ++ "/"
+    baseDir :: IO String
+    baseDir = do
+      dir <- fromMaybe "/tmp/togo/" <$> lookupEnv "DB_DIR"
+      putStrLn dir
+      pure dir
+
+    matchDir :: M.Match -> IO String
+    matchDir match = do
+      base <- baseDir
+      pure $ base ++ M.identifier match ++ "/"
 
     toMatchIfExists :: String -> IO (Maybe M.Match)
     toMatchIfExists matchId = do
       let match = M.Match matchId
-      exists <- doesDirectoryExist (matchDir match)
+      dir <- matchDir match
+      exists <- doesDirectoryExist dir
       pure $ if exists then Just match else Nothing
 
     createMatchAndDir :: IO M.Match
     createMatchAndDir = do
       guid <- genString
       let match = M.Match guid
-      exists <- doesDirectoryExist "/tmp/togo"
-      unless exists $ createDirectory "/tmp/togo"
-      createDirectory (matchDir match)
+      dir <- matchDir match
+      shelly $ mkdir_p $ fromText $ pack dir
       pure match
 
     saveEventInFile :: Event -> M.Match -> IO ()
     saveEventInFile event match = do
-      fileCount <- length <$> listDirectory (matchDir match)
-      encodeFile (matchDir match ++ show fileCount) event
+      dir <- matchDir match
+      fileCount <- length <$> listDirectory dir
+      encodeFile (dir ++ show fileCount) event
 
     readEventsFromFiles :: M.Match -> IO [Event]
     readEventsFromFiles match = do
-      (fileNumbers :: [Int]) <- mapMaybe readMaybe <$> listDirectory (matchDir match)
+      dir <- matchDir match
+      (fileNumbers :: [Int]) <- mapMaybe readMaybe <$> listDirectory dir
       let sorted = sortOn Ord.Down fileNumbers
-      sortedEventText <- mapM (decodeFileStrict . (++) (matchDir match) . show) sorted
+      sortedEventText <- mapM (decodeFileStrict . (++) dir . show) sorted
       case sequence sortedEventText of
         Just events -> pure events
         _ -> pure []
